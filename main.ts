ba89549ff439b484d2a04b49144d22c741d4776c
slykkkkkk <71689450+slykkkkkk@users.noreply.github.com>
2020-10-20 15:16:49 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,31 @@
-let x = 0
-let y = 0
-while (y >= 0) {
-    led.plot(x, y)
-    basic.pause(500)
-    if (y / 2 == "0") {
-        if (x > 0) {
-            x += -1
+let x = 4
+let y = 4
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (y >= 0) {
+        led.plot(x, y)
+        basic.pause(100)
+        if (y % 2 == 0) {
+            if (x > 0) {
+                x += -1
+            } else {
+                y += -1
+            }
         } else {
-            y += -1
-        }
-    } else {
-        if (x < 4) {
-            x += 1
-        } else {
-            y += -1
+            if (x < 4) {
+                x += 1
+            } else {
+                y += -1
+            }
         }
     }
-}
-basic.forever(function () {
-    x = 4
-    y = 4
+    if (x == 0 && y == 0) {
+        led.plot(0, 0)
+        basic.pause(500)
+        basic.clearScreen()
+        x = 4
+        y = 4
+    }
 })
